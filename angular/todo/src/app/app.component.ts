f8f59c856448bacd42a7ec1815f5f0ca008c3a60
monica88lima <flima.monica@gmail.com>
2023-12-24 16:37:41 -0300

    ajustando layout e incluindo regras das funcionalidades

--- angular/todo/src/app/app.component.ts
+++ angular/todo/src/app/app.component.ts
@@ -2,48 +2,84 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Todo } from '../models/todo.models';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: String = 'Meus Estudos';
- constructor() {
-  this.todos.push(new Todo(1,false,'Estudar Blazor'));
-  this.todos.push(new Todo(2,false,'Estudar Boostrap'));
-  this.todos.push(new Todo(3,false,'Estudar Angular'));
-  this.todos.push(new Todo(4,true,'Estudar JavaScript'));
+  public form: FormGroup;
+  public mode: String ='list';
 
- }
-  
-  alteraTexto(){
-    this.title = 'teste'
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required])]
+    });
+    this.carregar();
+
   }
-  removeTarefa(todo: Todo){
+
+  adicionarTarefa() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, false, title));
+    this.salvar();
+    this.limparTarefa();
+  }
+
+  limparTarefa() {
+    this.form.reset();
+  }
+
+
+  removeTarefa(todo: Todo) {
     const index = this.todos.indexOf(todo);
-    if(index != -1){
-      this.todos.splice(index,1)
+    if (index != -1) {
+      this.todos.splice(index, 1)
 
     }
-   
+    this.salvar();
+
 
   }
-  mudaStatusTrue(todo: Todo){
-  todo.done = true;
+  mudaStatusTrue(todo: Todo) {
+    todo.done = true;
+    this.salvar();
 
-        
+
   }
-  mudaStatusfalse(todo: Todo){
+  mudaStatusfalse(todo: Todo) {
     todo.done = false;
-  
-          
-    }
- 
- }
+    this.salvar();
+
+
+  }
+
+  salvar() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data)
+  }
+
+  carregar() {
+    const data = localStorage.getItem('todos');
+    if (data)
+      this.todos = JSON.parse(data);
+  }
+
+  trocarModo(mode: string){
+    this.mode = mode;
+  }
+}
 
 
