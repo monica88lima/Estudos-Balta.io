59852f5fd42473accb7f9f40de5081e61f9eeb51
monica88lima <flima.monica@gmail.com>
2023-12-26 10:51:45 -0300

    finalização estilo e regra da app

--- angular/todo/src/app/app.component.ts
+++ angular/todo/src/app/app.component.ts
@@ -17,7 +17,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class AppComponent {
   public todos: Todo[] = [];
   public form: FormGroup;
-  public mode: String ='list';
+  public mode ='list';
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -33,11 +33,12 @@ export class AppComponent {
   adicionarTarefa() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
-    this.todos.push(new Todo(id, false, title));
+    this.todos.push(new Todo(id, false, title.charAt(0).toUpperCase() + title.slice(1)));
     this.salvar();
     this.limparTarefa();
   }
 
+  
   limparTarefa() {
     this.form.reset();
   }
@@ -69,6 +70,7 @@ export class AppComponent {
   salvar() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data)
+    this.mode = 'list';
   }
 
   carregar() {
